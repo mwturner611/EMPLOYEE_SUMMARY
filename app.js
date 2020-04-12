6060acba650d84bd3d97a416703d768c620eb78b
mwturner611 <mwturner611@gmail.com>
2020-04-12 17:00:28 -0500

    main loop arranging function calls

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// create path to output folder for fs.writeFile
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// Write code to use inquirer to gather information about the development team members,
+// define function with inquirer to gather information about the development team members
 function questionBlock() {
     return inquirer.prompt([
         {
@@ -53,70 +54,14 @@ function questionBlock() {
         },
         {
             type: "list",
-            name: "finish",
+            name: "restart",
             message: "Do you have more team members to add?",
             choices: ["Yes","No"]
         },
     ])
 };
 
-
-// const matt = new Engineer("Matt",12,"matt@email.com","github.com");
-// const tricia = new Manager("Tricia",123,"Tricia@email.com",72);
-// const employees = [matt,tricia];
-
-
-
-
-// questionBlock()
-// .then(answers => {
-//     const employees = [];
-//     if (answers.finish === "Yes"){
-//         addToArray(answers);
-//         const htmlPage = render(employees)
-
-//         fs.writeFile(outputPath,htmlPage, function(err){
-//             if(err){
-//                 console.log(err)
-//             }
-//             else{
-//                 console.log("Generated HTML")
-//             }
-//         })    
-//     }
-//     else{
-//         console.log("let's ask more questions")
-//     }})
-// .catch((err) => {
-//     console.log(err);
-// })
-
-
-questionBlock()
-.then(answers => {
-    const employees = [];
-
-    let employee = addToArray(answers);
-
-    employees.push(employee);
-
-    const htmlPage = render(employees);
-
-    fs.writeFile(outputPath,htmlPage, function(err){
-        if(err){
-            console.log(err)
-        }
-        else{
-            console.log("Generated HTML");
-        }
-    })
-
-})
-.catch((err) => {
-    console.log(err);
-})
-
-
+// function adding one new class of EE
 const addToArray = (answers) =>{
     let employee = {};
     if (answers.role === "Manager"){
@@ -131,23 +76,38 @@ const addToArray = (answers) =>{
     return employee;
 }
 
-// and to create objects for each team member (using the correct classes as blueprints!)
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// 
+const mainLoop = (array) => {
+    
+    let employees = array;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    questionBlock()
+    .then(answers => {
+        // array for employees
+        let employee = addToArray(answers);
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+        employees.push(employee);
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!
+        if (answers.restart === "No"){
+            const htmlPage = render(employees);
+
+            fs.writeFile(outputPath,htmlPage, function(err){
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    console.log("Generated HTML in Outputs folder");
+                }
+            })
+        }
+        else{
+            return mainLoop(employees);
+            
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+mainLoop([]);
